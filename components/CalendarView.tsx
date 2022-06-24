bcf4b101c7ed3df05c1b923d91a17682fffbe39b
Jack Allen <38332643+jackall3n@users.noreply.github.com>
2022-06-24 16:35:26 +0100

    show now

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -174,7 +174,7 @@ export function CalendarView({ performances, user, users, onClick }) {
 
   useEffect(() => {
     if (showNow) {
-       ref.current.scrollTo(nowMargin - (ref.current.offsetWith / 2), 0)
+       ref.current.scrollTo(nowMargin - (ref.current.offsetWidth / 2), 0)
     }
     
     setTimeout(() => onScroll(ref.current));
